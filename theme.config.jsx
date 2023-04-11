bdcc4298d065c4450dc99f4b21d912107e256194
tidvn <tiendung0325@gmail.com>
2023-04-11 11:51:32 +0700

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const config = {
+  logo: <span>DPay Documenation</span>,
+  project: {
+    link: 'https://github.com/',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/',
+  footer: {
+    text: 'DPay@2023',
+  },
+}
+
+export default config
